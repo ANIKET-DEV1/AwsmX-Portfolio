58377f9189287699fe837dd12467bd270ba1fa40
ANIKET_DEV1 <ag9326107@gmail.com>
2026-04-01 22:16:22 +0530

    Vercel

--- api/index.py
+++ api/index.py
@@ -2,11 +2,11 @@ from datetime import datetime
 import os
 from flask import Flask, render_template
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-template_dir = os.path.join(base_dir, '../templates')
-static_dir = os.path.join(base_dir, '../static')
+# base_dir = os.path.dirname(os.path.abspath(__file__))
+# template_dir = os.path.join(base_dir, '../templates')
+# static_dir = os.path.join(base_dir, '../static')
 
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app = Flask(__name__)
 
 portfolio = {
     "name": "Aniket Gupta",
